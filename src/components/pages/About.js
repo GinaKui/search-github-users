6d0015f362afa4fde00dd94d83fd0226e4e69061
ginakui <developerkui@outlook.com>
2019-11-19 22:55:20 -0500

    fix anchor tag in About page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,9 +5,9 @@ const About = () => {
     <Fragment>
       <h1>About This App</h1>
       <p>User interface to search Github users</p>
-      <p>Version: 1.0.1</p>
-      <p>Source Code at: <a to='https://github.com/GinaKui/search-github-users'>github</a></p>
-      <p>The backend api document: <a>https://developer.github.com/v3/</a></p>
+      <p>Version: 1.0.2</p>
+      <p>Github <a href="https://github.com/GinaKui/search-github-users" target="_blank" rel="noopener noreferrer">Source Code</a></p>
+      <p>The backend api document can be found <a href="https://developer.github.com/v3/" target="_blank" rel="noopener noreferrer">here</a></p>
     </Fragment>
   );
 };
